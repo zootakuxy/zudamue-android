a3260b8ddd7bbca051028f9234b80476a8a70111
costa-xdaniel <costa.xdaniel@gmail.com>
2017-06-26 17:38:16 +0000

    alteracao

---
+++ zudamue/src/main/java/st/zudamue/support/android/adapter/RecyclerViewFilter.java
@@ -0,0 +1,11 @@
+package st.zudamue.support.android.adapter;
+
+/**
+ * Created by xdaniel on 6/26/17.
+ *
+ * @author Daniel Costa <costa.xdaniel@gmail.com/>
+ */
+
+public interface RecyclerViewFilter {
+    public boolean accept( ItemDataSet itemDataSet );
+}
